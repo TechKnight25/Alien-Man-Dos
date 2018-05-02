bc95884b5a7b44fdf76b9e4e1aa1423b14f2aed8
Max Ward <mward0002@stu.d214.org>
2018-05-02 12:19:37 -0500

    xD

--- WorkoutApp/MarkViewController.swift
+++ WorkoutApp/MarkViewController.swift
@@ -9,16 +9,10 @@
 import UIKit
 
 class MarkViewController: UIViewController {
-
+var mark = "badAtCoding"
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
